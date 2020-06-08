c8e96e66a67de323c2ae7f539560e8e930ac3b10
Muhammad Amir <mianamirlahore@gmail.com>
2020-06-08 17:07:19 +0500

    lists operations

--- lists.ex
+++ lists.ex
@@ -17,5 +17,38 @@ defmodule Lists do
         IO.inspect [34, 56], char_lists: :as_lists
 
 
+        Enum.each tail, fn item ->
+            IO.puts item
+        end
+
+        fruits = ["Mango", "Banana", "Grapes", "Orange"]
+
+        Enum.each fruits, fn fruit ->
+            IO.puts fruit
+        end
+
+
+        # using recursion
+        IO.puts "Using recurion"
+        display_list(fruits)
+        IO.puts "After delete an item"
+        IO.puts display_list(List.delete(fruits, "Mango"))
+        IO.puts display_list(List.delete_at(fruits, 1))
+        IO.puts display_list(List.delete_at(fruits, 4, "Mango"))
+
+        IO.puts List.first(fruits)
+        IO.puts List.last(fruits)
+
+        my_stats = [name: "Amir": height: 5.8]
+
+
     end
+
+    
+    def display_list([fruit|fruits]) do
+        IO.puts fruit
+        display_list(fruits)
+    end
+
+    def display_list([]), do: nil
 end
